6e6a6c207ccf0c2093dc9234cc387c48cdda2b97
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-04-08 19:03:33 +0200

    - added proxy to avoid CORS Error - added first version of RegisterUserForm.jsx fragment - added useCreateUser.jsx hook for create user post request to the backend

--- src/fragments/RegisterUserForm.jsx
+++ src/fragments/RegisterUserForm.jsx
@@ -1,22 +1,69 @@
-import {TextField, Button} from "@mui/material";
+import {TextField, Button, Grid, Alert} from "@mui/material";
 import {useState} from "react";
+import {Title} from "../components/Title";
+import {useCreateUser} from "../hooks/useCreateUser";
 
 
 export const RegisterUserForm = () => {
-    const [mailValue, setMailValue] = useState("")
 
-    const onMailChange = (e) => setMailValue(e.target.value)
-    const handleSubmit = () => console.log(mailValue)
+    const [registeredUser, setRegisteredUser] = useState(null)
+    const {loading, error, registerUser} = useCreateUser();
+    const [formData, setFormData] = useState({
+        name: '',
+        password: '',
+        mailAddress: '',
+    });
+
+    const handleSubmit = async (e) => {
+        //todo: add form validation before calling the hook
+        e.preventDefault();
+        const response = await registerUser(formData)
+        console.log(response)
+        setRegisteredUser(response)
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return <main>
+        <Title secondary>Register User</Title>
+        {registeredUser === null &&
+            <form>
+                <Grid container rowSpacing={3} >
+                    <Grid item xs={12}>
+                        <TextField name="name" required id="outlined-basic" label="Name" variant="outlined"
+                                   onChange={handleChange}
+                                   value={formData.name} fullWidth/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField name="mailAddress" required id="outlined-basic" label="Mail Address"
+                                   variant="outlined"
+                                   onChange={handleChange}
+                                   value={formData.mailAddress} fullWidth/>
+                    </Grid>
+                    <Grid item xs={12}>
 
-        <form onSubmit={handleSubmit}>
-
-            <TextField id="outlined-basic" label="Mail Address" variant="outlined" onChange={onMailChange}
-                       value={mailValue}/>
-            <TextField id="outlined-basic" label="Password" variant="outlined"/>
-
-            <Button variant={"contained"} type={"submit"}>Submit</Button>
-
-        </form>
+                        <TextField name="password" required id="outlined-basic" label="Password" variant="outlined"
+                                   onChange={handleChange}
+                                   value={formData.password} fullWidth/>
+                    </Grid>
+                    <Grid item container justify="center">
+                        <Button size="large" variant="contained" disabled={loading}
+                                onClick={handleSubmit}>Submit</Button>
+                    </Grid>
+                </Grid>
+            </form>
+        }
+        {error != null &&
+            <Alert variant="outlined" severity="error" style={{ marginTop: '16px' }}> An error occured: {error}</Alert>
+        }
+        {registeredUser != null &&
+            <Alert variant="outlined" severity="success" style={{ marginTop: '16px' }}> User {registeredUser.name} was successfully registered</Alert>
+            //todo: add button to continue or do something next
+        }
     </main>
 };
---
+++ src/hooks/useCreateUser.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+export const useCreateUser = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const registerUser = async (userDTO) => {
+        setLoading(true);
+
+        try {
+            const response = await axios.post('/v1/users/create', userDTO);
+            setLoading(false);
+           return response.data;
+        } catch (err) {
+            setLoading(false);
+            setError(err.response.data.error);
+            return null;
+        }
+    };
+
+    return { loading, error, registerUser };
+};
