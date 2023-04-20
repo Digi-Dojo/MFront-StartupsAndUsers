ef335206363e3431146edfffa567ab85520334d8
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-04-20 18:21:25 +0200

    added fixme to axios.js file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const baseURL = 'https://dog.ceo/api/breeds'
 
+//fixme: use this to declare default endpoint or delete it as it's not used atm
+
 export default axios.create({
   baseURL,
   headers: {
