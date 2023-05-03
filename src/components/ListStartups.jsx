0e00afa218f135f9b45aaf82c7d816b7933bbcf3
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-04 00:38:27 +0200

    testing #5

--- src/components/ListStartups.jsx
+++ src/components/ListStartups.jsx
@@ -1,9 +1,11 @@
 import {MenuItem} from "@mui/material";
 import React from "react";
+import {csCZ} from "@mui/material/locale";
 
 export const ListStartups = ({startups}) =>{
 
     const isArray = Array.isArray(startups);
+    console.log(startups)
     if(isArray){
         return(startups.map((startup) => (
             <MenuItem key={startup.id} value={startup}>
