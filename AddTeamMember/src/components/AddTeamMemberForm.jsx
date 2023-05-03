ab1bd0f7bc3f41e7594ce2eebd14c64aeaea558a
João Daniel <jotaf.daniel@gmail.com>
2023-05-03 11:21:33 +0200

    port add-team-member
    note: it still has to handle the dependency of User "fragment"
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ AddTeamMember/src/components/AddTeamMemberForm.jsx
@@ -0,0 +1,54 @@
+import {Title} from "./Title";
+import {User} from "../fragments/User";
+import {Alert, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import {ErrorAlert} from "./ErrorAlert";
+
+export const AddTeamMemberForm = ({startups, loggedUser, newTeamMember, formData, handleChange, handleSubmit, error, loading, disableSubmitButton, selectedStartup, handleSelectChange}) => {
+    return (
+        <main>
+            <Title secondary>Add Team Member</Title>
+            <User loggedUser={loggedUser}></User>
+            {newTeamMember === null &&
+                <form>
+                    <Grid container rowSpacing={3}>
+                        <Grid item xs={12}>
+                            <TextField name="role" required id="outlined-basic" label="Role" variant="outlined"
+                                       onChange={handleChange}
+                                       value={formData.password}
+                                       fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl variant="outlined" fullWidth>
+                                <InputLabel>Select a Startup</InputLabel>
+                                <Select
+                                    value={selectedStartup}
+                                    onChange={handleSelectChange}
+                                    label="Select a Startup"
+                                >
+                                    {startups.map((startup) => (
+                                        <MenuItem key={startup.id} value={startup}>
+                                            {startup.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item container justify="center">
+                            <Button size="large" variant="contained" disabled={loading || disableSubmitButton}
+                                    onClick={handleSubmit}
+                            >Add</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            }
+            <ErrorAlert error={error} />
+            {newTeamMember != null &&
+                <Alert variant="outlined" severity="success" style={{marginTop: '16px'}}>You were successfully added as
+                    a member to the startup {selectedStartup.name} with role {newTeamMember.role} </Alert>
+            }
+
+        </main>
+    );
+
+}
--- AddTeamMember/src/root.component.js
+++ AddTeamMember/src/root.component.js
@@ -1,3 +1,59 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import {useAllStartups} from "./hooks/useAllStartups";
+import {useContext, useEffect, useState} from "react";
+import {useCreate} from "./hooks/useCreate";
+import {UserContext} from "./components/UserContext";
+import {AddTeamMemberForm} from "./components/AddTeamMemberForm";
+
+export default function AddTeamMember() {
+    const startups = useAllStartups()
+    const [selectedStartup, setSelectedStartup] = useState("");
+    const [disableSubmitButton, setDisableSubmitButton] = useState(true)
+    const {loading, error, createNew} = useCreate()
+    const {loggedUser} = useContext(UserContext);
+    const [newTeamMember, setNewTeamMember] = useState(null);
+    const [formData, setFormData] = useState({
+        userId: loggedUser != null ? loggedUser.id : '',
+        role: '',
+        startupId: ''
+    });
+
+    useEffect(() => {
+        const newVal = loggedUser != null ? loggedUser.id : '';
+        setFormData({
+            ...formData,
+            userId: newVal,
+        });
+    }, [loggedUser]);
+
+
+    useEffect(() => {
+        setDisableSubmitButton(Object.values(formData).some(x => x === ''));
+    }, [formData]);
+
+    const handleSubmit = async (e) => {
+        //todo: add form validation before calling the hook
+        e.preventDefault();
+        const response = await createNew(formData, "teammembers/create")
+        setNewTeamMember(response)
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSelectChange = (e) => {
+        setSelectedStartup(e.target.value);
+        setFormData({
+            ...formData,
+            startupId: e.target.value.id,
+        })
+    };
+
+    return <AddTeamMemberForm disableSubmitButton={disableSubmitButton} error={error} loading={loading}
+                              loggedUser={loggedUser} handleSelectChange={handleSelectChange}
+                              selectedStartup={selectedStartup} newTeamMember={newTeamMember} startups={startups}
+                              formData={formData} handleChange={handleChange} handleSubmit={handleSubmit}/>
 }
