8a69a8a88583384d2302fa8675f2433f1d3f4497
João Daniel <jotaf.daniel@gmail.com>
2023-05-03 15:04:32 +0200

    port Logout
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- Logout/src/root.component.js
+++ Logout/src/root.component.js
@@ -1,3 +1,20 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import {useContext} from "react";
+import {UserContext} from "./components/UserContext";
+import {Button, Grid} from "@mui/material";
+
+export default function Logout() {
+    const {loggedUser, updateUser} = useContext(UserContext);
+
+    const handleLogout = () => {
+        updateUser(null)
+    }
+
+    return <main>
+        <form>
+            <Grid item xs={12} container justify="center">
+                <Button size="large" variant="contained" disabled={loggedUser === null}
+                        onClick={handleLogout}>Logout</Button>
+            </Grid>
+        </form>
+    </main>
 }
