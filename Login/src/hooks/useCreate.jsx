f67d0800ae839d264a960d367f9fbf3ce7cf2d0b
João Daniel <jotaf.daniel@gmail.com>
2023-05-03 15:02:55 +0200

    port Login
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ Login/src/hooks/useCreate.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+export const useCreate = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const createNew = async (operationDTO, path) => {
+        setLoading(true);
+
+        try {
+            const response = await axios.post('/v1/'+ path, operationDTO);
+            setLoading(false);
+           return response.data;
+        } catch (err) {
+            setLoading(false);
+            setError(err.response.data.error);
+            return null;
+        }
+    };
+
+    return { loading, error, createNew };
+};
--- Login/src/root.component.js
+++ Login/src/root.component.js
@@ -1,3 +1,42 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
-}
+import {useContext, useEffect, useState} from "react";
+import {useCreate} from "./hooks/useCreate";
+import {UserContext} from "./components/UserContext";
+import {LoginForm} from "./components/LoginForm";
+
+
+export default function Login() {
+    const [loggedUser, setLoggedUser] = useState(null)
+    const {loading, error, createNew} = useCreate()
+    const [disableSubmitButton, setDisableSubmitButton] = useState(false)
+    const {updateUser} = useContext(UserContext);
+    const [formData, setFormData] = useState({
+        password: '',
+        mailAddress: '',
+    });
+
+    useEffect(() => {
+        if (loggedUser) {
+            updateUser(loggedUser);
+        }
+    }, [loggedUser]);
+
+    useEffect(() => {
+        setDisableSubmitButton(Object.values(formData).some(x => x === ''));
+    }, [formData]);
+
+    const handleSubmit = async (e) => {
+        //todo: add form validation before calling the hook
+        e.preventDefault();
+        const response = await createNew(formData, "users/login")
+        setLoggedUser(response)
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    return <LoginForm disableSubmitButton={disableSubmitButton} error={error} loading={loading} loggedUser={loggedUser}
+                      formData={formData} handleChange={handleChange} handleSubmit={handleSubmit}/>
+};
