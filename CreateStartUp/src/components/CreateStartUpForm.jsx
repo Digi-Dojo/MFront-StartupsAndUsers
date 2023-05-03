2e8fee463bc5348d89785ee72fc841837c8a1f84
João Daniel <jotaf.daniel@gmail.com>
2023-05-03 14:57:35 +0200

    port CreateStartUp
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ CreateStartUp/src/components/CreateStartUpForm.jsx
@@ -0,0 +1,35 @@
+import {Title} from "./Title";
+import {Alert, Button, Grid, TextField} from "@mui/material";
+import {ErrorAlert} from "./ErrorAlert";
+
+export const CreateStartUpForm = ({registeredStartUp, error, loading, disableSubmitButton, formData, handleChange, handleSubmit}) => {
+    return <main>
+        <Title secondary>Create new Startup</Title>
+        {registeredStartUp === null &&
+            <form>
+                <Grid container rowSpacing={3} >
+                    <Grid item xs={12}>
+                        <TextField name="name" required id="outlined-basic" label="Name" variant="outlined"
+                                   onChange={handleChange}
+                                   value={formData.name} fullWidth/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField name="description" required id="outlined-basic" label="description"
+                                   variant="outlined"
+                                   onChange={handleChange}
+                                   value={formData.description} fullWidth/>
+                    </Grid>
+                    <Grid item container justify="center">
+                        <Button size="large" variant="contained" disabled={loading || disableSubmitButton}
+                                onClick={handleSubmit}>Submit</Button>
+                    </Grid>
+                </Grid>
+            </form>
+        }
+        <ErrorAlert error={error} />
+        {registeredStartUp != null &&
+            <Alert variant="outlined" severity="success" style={{ marginTop: '16px' }}> Startup {registeredStartUp.name} was successfully created</Alert>
+            //todo: add button to continue or do something next
+        }
+    </main>
+}
