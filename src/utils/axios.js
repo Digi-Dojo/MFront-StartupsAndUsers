238e04e42d948dafd5b8adbb355ee526d81382da
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-08 23:13:04 +0200

    changed base url to render backend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8080/";
+const baseURL = "https://usersandteams.onrender.com/";
 
 //fixme: use this to declare default endpoint or delete it as it's not used atm
 
