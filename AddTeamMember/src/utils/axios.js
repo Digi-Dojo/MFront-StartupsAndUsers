a55e1ae01d58493a03c20a2b8140fec679686b11
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-13 19:57:11 +0200

    updated src in order to be complient with master

---
+++ AddTeamMember/src/utils/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:8080'
+
+//fixme: use this to declare default endpoint or delete it as it's not used atm
+
+export default axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
