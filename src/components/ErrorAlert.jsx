648008f9ef3120d56feb2460f758159008b6fb9c
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-17 22:52:04 +0200

    deleted old project

--- src/components/ErrorAlert.jsx
+++
@@ -1,8 +0,0 @@
-import {Alert} from "@mui/material";
-
-export const ErrorAlert = ({error}) => {
-    if (error !== null) {
-        return <Alert variant="outlined" severity="error" style={{marginTop: '16px'}}> An error
-            occured: {error}</Alert>
-    }
-}
