9d7923a3b2005492f332f46108395bd117abf398
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-17 23:47:31 +0200

    updated backend url from local to deployed one

--- ListUsersFromStartup/src/utils/axios.js
+++ ListUsersFromStartup/src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = 'https://usersandteams.onrender.com/'
+const baseURL = 'http://https://usersandteams.onrender.com/'
+
+//fixme: use this to declare default endpoint or delete it as it's not used atm
 
 export default axios.create({
   baseURL,
