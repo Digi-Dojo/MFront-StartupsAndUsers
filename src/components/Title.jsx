9ab3af6e1c59f77984b81c15c0efbf08a7e34fde
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-05-17 22:52:04 +0200

    deleted old project

--- src/components/Title.jsx
+++
@@ -1,5 +0,0 @@
-export const Title = (props) => {
-  if (props.primary) return <h1>{ props.children }</h1>
-  if (props.secondary) return <h2>{ props.children }</h2>
-  else return <strong>{ props.children }</strong>
-}
--- src/fragments/User.jsx
+++
@@ -1,8 +0,0 @@
-import {useContext} from "react";
-import {UserContext} from "../components/UserContext";
-import {UserComponent} from "../components/UserComponent";
-
-export const User = () => {
-    const {loggedUser} = useContext(UserContext);
-    return <UserComponent loggedUser={loggedUser} />
-}
