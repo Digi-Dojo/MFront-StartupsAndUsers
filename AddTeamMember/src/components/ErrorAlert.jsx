b2bf0592811b9c3400df31c1a54bd427bc7cc572
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-06-07 12:36:29 +0200

    fixed microfrontends

--- AddTeamMember/src/components/ErrorAlert.jsx
+++ AddTeamMember/src/components/ErrorAlert.jsx
@@ -4,5 +4,7 @@ export const ErrorAlert = ({error}) => {
     if (error !== null) {
         return <Alert variant="outlined" severity="error" style={{marginTop: '16px'}}> An error
             occured: {error}</Alert>
+    }else{
+        return <></>
     }
 }
--- RegisterUser/src/startup-digi-dojo-lab-register-user.js
+++ RegisterUser/src/startup-digi-dojo-lab-register-user.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
+import RegisterUser from "./root.component";
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root,
+  rootComponent: RegisterUser,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
