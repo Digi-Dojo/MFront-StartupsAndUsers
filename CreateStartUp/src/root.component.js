fedb6574ce8ef9e8e053289f7bda35f9725f500f
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-06-07 10:24:27 +0200

    trying something else

--- CreateStartUp/src/root.component.js
+++ CreateStartUp/src/root.component.js
@@ -3,6 +3,7 @@ import {useCreate} from "./hooks/useCreate";
 import Title from "./components/Title";
 import {Alert, Button, Grid, TextField} from "@mui/material";
 import ErrorAlert from "./components/ErrorAlert";
+import CreateStartUpForm from "./components/CreateStartUpForm";
 
 
 const CreateStartUp = () => {
@@ -34,35 +35,8 @@ const CreateStartUp = () => {
     }, [formData]);
 
     return (
-        <div>
-            <Title secondary>Create new Startup</Title>
-            {createdStartUp === null &&
-                <form>
-                    <Grid container rowSpacing={3} >
-                        <Grid item xs={12}>
-                            <TextField name="name" required id="outlined-basic" label="Name" variant="outlined"
-                                       onChange={handleChange}
-                                       value={formData.name} fullWidth/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField name="description" required id="outlined-basic" label="description"
-                                       variant="outlined"
-                                       onChange={handleChange}
-                                       value={formData.description} fullWidth/>
-                        </Grid>
-                        <Grid item container justify="center">
-                            <Button size="large" variant="contained" disabled={loading || disableSubmitButton}
-                                    onClick={handleSubmit}>Submit</Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            }
-            <ErrorAlert error={error} />
-            {createdStartUp != null &&
-                <Alert variant="outlined" severity="success" style={{ marginTop: '16px' }}> Startup {createdStartUp.name} was successfully created</Alert>
-                //todo: add button to continue or do something next
-            }
-        </div>
+        <CreateStartUpForm disableSubmitButton={disableSubmitButton} error={error} loading={loading} registeredStartUp={createdStartUp}
+                           formData={formData} handleChange={handleChange} handleSubmit={handleSubmit}/>
     )
 };
 
