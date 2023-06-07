39c0aebd5be492f3b2ef2b877c49103e6667d8e5
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-06-07 12:36:29 +0200

    fixed microfrontends

--- AddTeamMember/src/startup-digi-dojo-lab-add-team-member.js
+++ AddTeamMember/src/startup-digi-dojo-lab-add-team-member.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
-import Root from "./root.component";
+import {AddTeamMember} from "./root.component";
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root,
+  rootComponent: AddTeamMember,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
